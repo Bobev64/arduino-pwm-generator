0a74a194abccfd2fcebe829b5a6223399b1864ef
bobev64 <68569762+Bobev64@users.noreply.github.com>
2024-05-16 13:17:02 -0400

    Use servo control library instead

--- arduino-pwm-generator.ino
+++ arduino-pwm-generator.ino
@@ -1,36 +1,17 @@
-int outputpin=9; /* Assign symbolic name outputpin to D9 PWM pin of Arduino */
+#include <Servo.h>
 
-/* Perform initialization and declarations inside setup() */
-void setup()
-{
-  pinMode(outputpin, OUTPUT); /* set as a output put */
-  TCCR1A=_BV(COM1A1)|_BV(COM1B1); /* set Fast PWM Mode */
-  TCCR1B=_BV(WGM13)|_BV(CS11); /* Activate PWM Phase, frequency correction Mode */
+Servo motorController;  // create servo object to control a servo
+
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+
+void setup() {
+  motorController.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
-void loop()
-{
-  float frequency=0; /* initially set frequency to zero */
-  float count=10000;
-  float count2=0;
-  float readinput=0;
-  
-  while(1)
-  {
-    ICR1=count;
-    count2=2*8*count;
-    frequency= int(16000000/count2);
-    OCR1A=int(count/2);
-    
-    count=10000;
-    readinput=analogRead(A0);
-    readinput=(readinput/0.0113);
-    count=count+readinput;
-    
-    if(count>=100000)
-      {
-        count=10000;
-      }
-      delay(1000);
-    }
+void loop() {
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  motorController.write(val);                  // sets the servo position according to the scaled value
+  delay(15);                           // waits for the servo to get there
 }
